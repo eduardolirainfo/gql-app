a38c605b21be5c88d96175a13e189f12058c318f
eduardolirainfo <eduardolirainfo@gmail.com>
2025-02-16 18:44:44 -0300

    entities and query class

--- gql_app/app.py
+++ gql_app/app.py
@@ -1,30 +1,103 @@
 from fastapi import FastAPI
-from graphene import ObjectType, Schema, String
+from graphene import Field, Int, List, ObjectType, Schema, String
 from starlette_graphene3 import (
     GraphQLApp,
     make_graphiql_handler,
-    make_playground_handler,
 )
+
+employers_data = [
+    {'id': 1, 'name': 'Apple', 'email': 'demo1@gmail.com', 'industry': 'Tech'},
+    {
+        'id': 2,
+        'name': 'Money',
+        'email': 'demo2@gmail.com',
+        'industry': 'Finance',
+    },
+    {
+        'id': 3,
+        'name': 'Amazon',
+        'email': 'demo3@gmail.com',
+        'industry': 'Tech',
+    },
+    {'id': 4, 'name': 'Life', 'email': 'demo4@gmai.com', 'industry': 'Health'},
+]
+
+jobs_data = [
+    {
+        'id': 1,
+        'title': 'Software Engineer',
+        'description': 'Python Developer',
+        'employer_id': 1,
+    },
+    {
+        'id': 2,
+        'title': 'Software Engineer',
+        'description': 'JavaScript Developer',
+        'employer_id': 2,
+    },
+    {
+        'id': 3,
+        'title': 'Software Engineer',
+        'description': 'React Developer',
+        'employer_id': 3,
+    },
+    {
+        'id': 4,
+        'title': 'Software Engineer',
+        'description': 'Java Developer',
+        'employer_id': 4,
+    },
+]
+
+
+class EmployerObject(ObjectType):
+    id = Int()
+    name = String()
+    email = String()
+    industry = String()
+    jobs = List(lambda: JobObject)
+
+    @staticmethod
+    def resolve_jobs(root, info):
+        return [job for job in jobs_data if job['employer_id'] == root['id']]
+
+
+class JobObject(ObjectType):
+    id = Int()
+    title = String()
+    description = String()
+    employer_id = Int()
+    employer = Field(lambda: EmployerObject)
+
+    @staticmethod
+    def resolve_employer(root, info):
+        return next(
+            employer
+            for employer in employers_data
+            if employer['id'] == root['employer_id']
+        )
 
 
 class Query(ObjectType):
-    hello = String(name=String(default_value='graphql'))
+    employers = List(EmployerObject)
+    jobs = List(JobObject)
 
-    def resolve_hello(root, info, name):
-        return f'Hello {name}'
+    @staticmethod
+    def resolve_jobs(root, info):
+        return jobs_data
+
+    @staticmethod
+    def resolve_employers(root, info):
+        return employers_data
 
 
 schema = Schema(query=Query)
 
 app = FastAPI()
+
 app.mount(
     '/gql',
     GraphQLApp(schema=schema, on_get=make_graphiql_handler()),
-)
-
-app.mount(
-    '/gql-p',
-    GraphQLApp(schema=schema, on_get=make_playground_handler()),
 )
 
 # @app.get('/')

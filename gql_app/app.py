0bc10ad6645beb49c0ed7a15d61fc7d5f9e7ba0a
eduardolirainfo <eduardolirainfo@gmail.com>
2025-02-15 21:59:06 -0300

    post http client

---
+++ gql_app/app.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from graphene import ObjectType, Schema, String
+from starlette_graphene3 import GraphQLApp
+
+
+class Query(ObjectType):
+    hello = String(name=String(default_value='graphql'))
+
+    def resolve_hello(root, info, name):
+        return f'Hello {name}'
+
+
+schema = Schema(query=Query)
+
+app = FastAPI()
+app.mount('/gql', GraphQLApp(schema=schema))
+
+
+# @app.get('/')
+# def read_root():
+#     return {'Hello': 'World'}
+
+
+# @app.get('/hello/{name}')
+# def read_item(name: str):
+#     return {'Hello': name}
+
+
+# @app.get('/error')
+# def read_error():
+#     return JSONResponse(status_code=500, content={'error': 'Server error'})
